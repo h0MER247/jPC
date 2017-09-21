75f584a11ed9cff948320d21fcf916cd7b87d17b
h0MER247 <h0MER247@users.noreply.github.com>
2017-09-21 17:28:54 +0200

    Fixed author name. Something is not right with my NetBeans configuration. I wanted to remove this license information headers in the near future anyways - but until then...

--- src/Hardware/CPU/Intel80386/Instructions/FPU/Datatransfer/FLDZ.java
+++ src/Hardware/CPU/Intel80386/Instructions/FPU/Datatransfer/FLDZ.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 homer
+ * Copyright (C) 2017 h0MER247
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
